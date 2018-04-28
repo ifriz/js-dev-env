c4138f436b6d81c537500bd420b89d4527629ab5
Ian Frizzell <ian.frizzell@gmail.com>
2018-04-28 09:59:17 -0600

    Module HTTP - Added fetch calls and begin mocking http requests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './index.css';
+import {getUsers} from "./api/userApi";
+
+getUsers().then(results => {
+  let userBody = "";
+
+  results.forEach(user => {
+    // language=HTML
+    userBody += `<tr>
+  <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+  <td>${user.id}</td>
+  <td>${user.firstname}</td>
+  <td>${user.lastname}</td>
+  <td>${user.email}</td>
+</tr>`
+  });
+  global.document.getElementById('users').innerHTML = userBody;
+});
