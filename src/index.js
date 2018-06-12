c417ff99f69a79467b64c535adc908956d3d631b
Ian Frizzell <ian.frizzell@gmail.com>
2018-06-12 07:17:12 -0600

    Module HTTP - added mock API calls and data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from "./api/userApi";
+import {deleteUser, getUsers} from "./api/userApi";
 
 getUsers().then(results => {
   let userBody = "";
@@ -8,10 +8,23 @@ getUsers().then(results => {
     userBody += `<tr>
   <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
   <td>${user.id}</td>
-  <td>${user.firstname}</td>
-  <td>${user.lastname}</td>
-  <td>${user.email}</td>
+  <td>${user.firstName}</td>
+  <td>${user.lastName}</td>
+  <td>${user.email}</td> 
 </tr>`
   });
   global.document.getElementById('users').innerHTML = userBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes['data-id'].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+
 });
